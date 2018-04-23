505c6e09ebf1e669922eb4c4fd3b5651813be975
HuiG33 <HuiG33@github.com>
2018-04-23 19:29:45 +0300

    image from library or camera

--- RecycleApp/AddItemViewController.swift
+++ RecycleApp/AddItemViewController.swift
@@ -8,8 +8,54 @@
 
 import UIKit
 
-class AddItemViewController: UIViewController {
-
+class AddItemViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var selectedImage: UIImageView!
+    
+    @IBAction func takePicture(_ sender: UIButton) {
+        
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                image.sourceType = .camera
+                self.present(image, animated: true, completion: nil)
+            } else {
+                print("No available camera")
+            }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Library", style: .default, handler: { (action:UIAlertAction) in
+            image.sourceType = .photoLibrary
+            self.present(image, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil ))
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
+    {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            selectedImage.image = image
+        }
+        else
+        {
+            //Error message
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
